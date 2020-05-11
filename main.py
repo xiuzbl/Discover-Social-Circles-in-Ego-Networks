8c32cdcfa270874b5ca879cd0c15abdbe7f6bb55
xiuzbl <49602540+xiuzbl@users.noreply.github.com>
2020-05-11 22:50:30 +0800

    Update main.py

--- main.py
+++ main.py
@@ -6,15 +6,16 @@ def experiment(gd,K,lam,reps,gradientReps,improveReps):
     bestll = 0
     seed = 42
     edgeFeatures,edge_set,nEdgeFeatures,nNodes,clusters,NodeIndex = gd.file_process()
-    print(NodeIndex)
+    print(NodeIndex,nEdgeFeatures,nNodes,clusters)
     C = Cluster(K,reps,gradientReps,improveReps,lam,seed,edgeFeatures,
-                edge_set,nEdgeFeatures,nNodes,clusters,whichLoss='ZEROONE')
+                edge_set,nEdgeFeatures,nNodes,clusters,whichLoss='SYMMETRICDIFF')
     print('cluster',C.cluster)
-    nseeds = 3 # Number of random restarts
+    nseeds = 1 # Number of random restarts
     for seed in range(nseeds):
         seed+=1
         C.train()
         ll = C.loglikelihood(C.theta,C.alpha,C.chat)
+        print(C.chat)
         if ll>bestll or bestll==0:
             bestll = ll
             bestClusters = C.chat
@@ -40,10 +41,3 @@ edgefile = ID+'.edges'
 
 gd = graphData(nodefile, selffeatfile, clusfile, edgefile, 'FRIEND')
 experiment(gd,3,1,25,50,5)
-
-
-
-
-
-
-
